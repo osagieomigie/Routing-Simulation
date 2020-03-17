1681715a751ff2aa5f5dcb563b64dbcee9843511
Osagie Omigie <osagieomigie@gmail.com>
2020-03-16 23:49:50 -0600

    Reads and stores call workload

--- main.cpp
+++ main.cpp
@@ -1,42 +1,52 @@
 #include <iostream>
 #include <vector>
 #include <map>
+#include <string>
 #include<sstream>
 
 using namespace std;
-/* Global matrix data structure for network topology/state information */
-// int propdelay[MAX_ROW][MAX_COL];
-// int capacity[MAX_ROW][MAX_COL];
-// int available[MAX_ROW][MAX_COL];
-// int cost[MAX_ROW][MAX_COL];
 
 /* Event record */
-// struct Event
-// {
-//     float event_time;
-//     int event_type;
-//     int callid;
-//     char source;
-//     char destination;
-//     float duration;
-// } EventList[MAX_EVENTS];
+struct Event
+{
+    float event_time;
+    int event_type;
+    int callid;
+    char source;
+    char destination;
+    float duration;
+} typedef EventList; 
 
 int main()
 {
     map<char, map<char, vector<int> > > networkTopology;
     map<char, map<char, vector<int> > >::iterator networkIt;
+    map<string, int> capacity;
+    map<string, int> available;
+    vector<Event> workLoad;
 
     FILE *fp1 = fopen("topology.dat", "r");
     int i = 0;
     char src, dst;
     int delay, cap;
+    string link;
 
     while ((i = fscanf(fp1, "%c %c %d %d\n", &src, &dst, &delay, &cap)) == 4)
     {
+        // keep track of link capacity and availablity 
+        link = "";
+        link += src;
+        link += "-";
+        link += dst;
+
+        capacity.insert(pair<string,int>(link, cap));
+        available.insert(pair<string,int>(link, cap));
+
         if (networkTopology.find(src) == networkTopology.end())
         {
             map<char, vector<int> > tmpMap;
             vector<int> tmpVect;
+
             // add propagation delay, and capacity to vector
             tmpVect.push_back(delay);
             tmpVect.push_back(cap);
@@ -56,18 +66,25 @@ int main()
             networkTopology[src].insert(pair<char, vector<int> >(dst, tmpVect));
         }
 
-        // row = src - 'A';
-        // col = dst - 'A';
-        // propdelay[row][col] = delay;
-        // propdelay[col][row] = delay;
-        // capacity[row][col] = cap;
-        // capacity[col][row] = cap;
-        // available[row][col] = cap;
-        // available[col][row] = cap;
+        
     }
     fclose(fp1);
 
+
     /* Next read in the calls from "callworkload.dat" and set up events */
+    i = 0; 
+    float dur, arr;
+    FILE *fp2 = fopen("callworkload.dat", "r");
+    while ((i = fscanf(fp2, "%f %c %c %f\n", &arr, &src, &dst, &dur)) == 4){
+        //cout << "Duration: " << arr <<endl;
+        EventList tmpEvent; 
+        tmpEvent.event_time = arr;
+        tmpEvent.source = src;
+        tmpEvent.destination = dst;
+        tmpEvent.duration = dur;
+        workLoad.push_back(tmpEvent);
+    }
+    fclose(fp2);
 
     /* Now simulate the call arrivals and departures */
     // while (numevents > 0)
@@ -85,9 +102,9 @@ int main()
     // }
     /* Print final report here */
 
-    for(auto const &ent1 : networkTopology) {
-        auto const &outer_key = ent1.first;
-        cout << outer_key<< endl;
-    }
+    // for(auto const &ent1 : networkTopology) {
+    //     auto const &outer_key = ent1.first;
+    //     cout << outer_key<< endl;
+    // }
 
 }
