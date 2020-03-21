c484829545f1f5af8f87fcd41769e23ec547531c
Osagie Omigie <osagieomigie@gmail.com>
2020-03-20 20:12:42 -0600

    Refactored main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <stack>
 #include <cstring>
 #include <sstream>
+#include "djikstra.hpp"
 
 using namespace std;
 
@@ -42,81 +43,6 @@ float shpfDelay = 0;
 bool compareEvent(const EventList &first, const EventList &second)
 {
   return (first.event_time < second.event_time);
-}
-
-/* A utility function to find the vertex with minimum distance
-value, from the set of vertices not yet included in shortest
-path tree */
-int minDistance(int dist[], bool sptSet[])
-{
-  // Initialize min value
-  int min = INT_MAX, min_index;
-
-  for (int v = 0; v < MATRIX_SIZE; v++){
-    if (sptSet[v] == false && dist[v] <= min){
-      min = dist[v], min_index = v;
-    }
-  }
-
-  return min_index;
-}
-
-stack<int> djikstras(int graph[MATRIX_SIZE][MATRIX_SIZE], char src, char dst)
-{
-  int dist[MATRIX_SIZE];
-  bool setPath[MATRIX_SIZE];
-  int previous[MATRIX_SIZE];
-
-  for (int i = 0; i < MATRIX_SIZE; i++)
-  {
-    dist[i] = INT_MAX;
-    setPath[i] = false;
-    previous[i] = INT_MAX;
-  }
-  dist[src - 'A'] = 0; // distance to it self is 0 
-
-  //find the path from src to dst 
-  for (int count = 0; count < MATRIX_SIZE - 1; count++)
-  {
-    int u = minDistance(dist, setPath);
-
-    setPath[u] = true;
-
-    //if U is the DST then early exit
-    // if (u == dst - 'A')
-    // {
-    //   break;
-    // }
-
-    for (int v = 0; v < MATRIX_SIZE; v++)
-    {
-      if (!setPath[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
-      {
-        dist[v] = dist[u] + graph[u][v];
-        previous[v] = u;
-      }
-    }
-  }
-
-  stack<int> path;
-  int u = dst - 'A';
-  if (previous[u] != INT_MAX)
-  {
-    while (u != INT_MAX)
-    {
-      path.push(u);
-      u = previous[u];
-    }
-  }
-
-  // while (!path.empty())
-  // {
-  //     cout << path.top() << ' ';
-  //     path.pop();
-  // }
-  // cout << endl;
-
-  return path;
 }
 
 // Determines if a call event can be routed 
