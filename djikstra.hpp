5771d5e8e918fcaadab57a8a6c659a7663d70ffb
Osagie Omigie <osagieomigie@gmail.com>
2020-03-23 00:02:48 -0600

    Fully implemented SHPF, SDPF, and LLP

--- djikstra.hpp
+++ djikstra.hpp
@@ -16,16 +16,17 @@ int minDistance(int dist[], bool sptSet[])
 {
   // Initialize min value
   int min = INT_MAX, min_index;
+
   for (int v = 0; v < MATRIX_SIZE; v++){
     if (sptSet[v] == false && dist[v] <= min){
       min = dist[v], min_index = v;
     }
   }
-  
+
   return min_index;
 }
 
-stack<int> djikstras(int graph[MATRIX_SIZE][MATRIX_SIZE], char src, char dst, string algo)
+stack<int> djikstras(int graph[MATRIX_SIZE][MATRIX_SIZE], char src, char dst)
 {
   int dist[MATRIX_SIZE];
   bool setPath[MATRIX_SIZE];
@@ -45,6 +46,13 @@ stack<int> djikstras(int graph[MATRIX_SIZE][MATRIX_SIZE], char src, char dst, st
     int u = minDistance(dist, setPath);
 
     setPath[u] = true;
+
+    //if U is the DST then early exit
+    if (u == dst - 'A')
+    {
+      break;
+    }
+    //if (dist[u] == INT_MAX) continue;
 
     for (int v = 0; v < MATRIX_SIZE; v++)
     {
@@ -67,15 +75,9 @@ stack<int> djikstras(int graph[MATRIX_SIZE][MATRIX_SIZE], char src, char dst, st
     }
   }
 
-  // while (!path.empty())
-  // {
-  //     cout << path.top() << ' ';
-  //     path.pop();
-  // }
-  // cout << endl;
-
+    //cout << "path length: " << path.size()<< endl; 
   return path;
-}
+} 
 
 /* A utility function to find the vertex with minimum distance
 value, from the set of vertices not yet included in shortest
@@ -124,6 +126,75 @@ stack<int> llpDjikstras(int graph[MATRIX_SIZE][MATRIX_SIZE], int capacity[MATRIX
         if (!setPath[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + load < dist[v]) {
             dist[v] = max(dist[u], dist[v]);
             dist[v] = dist[u] + load;
+            previous[v] = u;
+        }
+    }
+  }
+
+  stack<int> path;
+  int u = dst - 'A';
+  if (previous[u] != INT_MAX)
+  {
+    while (u != INT_MAX){
+      path.push(u);
+      u = previous[u];
+    }
+  }
+
+  return path;
+}
+
+/* A utility function to find the vertex with minimum distance
+value, from the set of vertices not yet included in shortest
+path tree */
+int min2Distance(double dist[], bool sptSet[])
+{
+  // Initialize min value
+  int min = -1, min_index;
+  for (int v = 0; v < MATRIX_SIZE; v++){
+    if (sptSet[v] == false && dist[v] <= min){
+      min = dist[v], min_index = v;
+    }
+  }
+  
+  return min_index;
+}
+
+stack<int> mfcDjikstras(int graph[MATRIX_SIZE][MATRIX_SIZE], int capacity[MATRIX_SIZE][MATRIX_SIZE], char src, char dst, string algo)
+{
+    string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    double dist[MATRIX_SIZE];
+    bool setPath[MATRIX_SIZE];
+    int previous[MATRIX_SIZE];
+    double load = 0; 
+
+  for (int i = 0; i < MATRIX_SIZE; i++)
+  {
+    dist[i] = -1;
+    setPath[i] = false;
+    previous[i] = -1;
+  }
+  dist[src - 'A'] = 0; // distance to it self is 0 
+
+  //find the path from src to dst 
+  for (int count = 0; count < MATRIX_SIZE - 1; count++)
+  {
+    int u = min2Distance(dist, setPath); 
+    setPath[u] = true;
+    
+    for (int v = 0; v < MATRIX_SIZE; v++)
+    { 
+        load = 0;
+        if (graph[u][v] <= 0){
+            load = (double) capacity[u][v];
+        }else
+        {
+            load = min(dist[v],dist[u]);
+        }
+        
+
+        if (!setPath[v] && graph[u][v] && dist[u] != -1 && dist[u] + load < dist[v]) {
+            dist[v] = dist[v] = dist[u] + load; //min(dist[u], dist[v]);
             previous[v] = u;
         }
     }
--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@ using namespace std;
 #define CALL_ARRIVAL 0
 #define CALL_END 1
 #define MATRIX_SIZE 26
-#define TOPOLOGY_DATA "top_sina.dat"//"topology_small.dat"
-#define WORKLOAD_DATA "work_sina.dat" //"callworkload_small.dat"
+#define TOPOLOGY_DATA   "topology.dat" //"topology_small.dat"  "top_sina.dat" 
+#define WORKLOAD_DATA    "callworkload.dat" //"callworkload_small.dat" "work_sina.dat"
 
 /* Event record */
 struct Event
@@ -33,10 +33,13 @@ int propdelay[MATRIX_SIZE][MATRIX_SIZE];
 int capacity[MATRIX_SIZE][MATRIX_SIZE];
 int available[MATRIX_SIZE][MATRIX_SIZE];
 int cost[MATRIX_SIZE][MATRIX_SIZE];
+int reset[MATRIX_SIZE][MATRIX_SIZE];
 list<EventList> workLoad;
 map<int, stack<int> > routesUsed;
-float shpfHops = 0, sdpfHops = 0, llpHops =0; 
-float shpfDelay = 0, sdpfDelay = 0, llpDelay =0; 
+
+// variables needed for the average 
+double shpfHops = 0, sdpfHops = 0, llpHops =0, mfcHops =0; 
+double shpfDelay = 0, sdpfDelay = 0, llpDelay =0, mfcDelay=0; 
 
 // custom link comparison; for sorting events
 bool compareEvent(const EventList &first, const EventList &second)
@@ -44,22 +47,68 @@ bool compareEvent(const EventList &first, const EventList &second)
   return (first.event_time < second.event_time);
 }
 
+//used in SHPF, and SDPF to determine shortest available path 
+void resetCost(int graph[MATRIX_SIZE][MATRIX_SIZE], string algo){
+  
+  for (int i = 0; i < MATRIX_SIZE; i++){
+    for (int j = 0; j < MATRIX_SIZE; j++){
+      if (algo == "SHPF"){
+        if (available[i][j] > 0){ // reset link availability
+          graph[i][j] = 1;
+        }else{
+          graph[i][j] = 0;
+        }
+      }
+      else if (algo == "SDPF"){
+        if (available[i][j] > 0){
+          graph[i][j] = propdelay[i][j];
+        }else{
+          graph[i][j] = 0;
+        }
+      }
+      else if (algo == "RESET")
+      {
+        if (reset[i][j] > 0){
+          graph[i][j] = reset[i][j];
+        }
+      }
+      
+    }
+  }
+}
+
 // Determines if a call event can be routed 
 int RouteCall(char source, char destination, EventList currentEvent, int resource[MATRIX_SIZE][MATRIX_SIZE], string algo)
 {
   stack<int> path;
-  if (algo == "LLP"){
-    path = llpDjikstras(resource, capacity, source, destination, algo); 
-    if (path.size() == 0){
-      return 0; // call is blocked 
-    }
-  }else
-  {
-    path = djikstras(resource, source, destination, algo);
+
+  //get path for each policy 
+  if (algo == "SHPF"){
+    resetCost(cost, "SHPF"); // reset cost graph 
+    path = djikstras(resource, source, destination);
+    
   }
-  
-   
-  stack<int> route;
+  else if (algo == "SDPF")
+  {
+    int delayCost[MATRIX_SIZE][MATRIX_SIZE];
+    resetCost(delayCost, "SDPF");
+    path = djikstras(delayCost, source, destination);
+  }
+  else if (algo == "LLP"){
+    path = llpDjikstras(resource, capacity, source, destination, algo); 
+  }
+  else if (algo == "MFC")
+  {
+    path = mfcDjikstras(resource, capacity, source, destination, algo); 
+  }
+
+  // no path found 
+  if (path.size() == 0){
+    return 0; // call is blocked 
+  }
+
+  stack<int> route = path;
+  stack<int> tmproute = path;
   string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   string link;
 
@@ -68,9 +117,10 @@ int RouteCall(char source, char destination, EventList currentEvent, int resourc
 
   string s, d; 
 
-  srcNode = path.top();  //s = alpha[srcNode];
+  srcNode = path.top();  s = alpha[srcNode];
   path.pop();
 
+  //determine if there are any unavailable links in path
   while(!path.empty())
   {
     dstNode = path.top(); d = alpha[dstNode];
@@ -78,48 +128,47 @@ int RouteCall(char source, char destination, EventList currentEvent, int resourc
 
     link = s + d;
     //cout <<"Link: " << link << endl;
-    route.push(srcNode); // add link to event route 
-    route.push(dstNode);
     
     // check if a link taken by call is at capacity 
-    if ((available[srcNode][dstNode] <= 0) && (available[dstNode][srcNode] <= 0)){
-      if (!route.empty()){
-        srcNode1 = route.top();
-        route.pop(); 
-      }
-      while(!route.empty()){
-        dstNode1 = route.top();
-        route.pop();
-
-        // restore capacity back to link 
-        available[srcNode1][dstNode1] = available[srcNode1][dstNode1] +1; 
-        available[dstNode1][srcNode1] = available[dstNode1][srcNode1] +1;
-
-        srcNode1 = dstNode1; 
-      }
-      //cout << "Blocked "<< endl;
+    if ((available[srcNode][dstNode] == 0) || (available[dstNode][srcNode] == 0)){
       return 0; // Call can't be completed 
-        
-    }
-    
-    // reduce capacity by one 
-    //cout << "link cap  1: " << available[srcNode][dstNode] << " link cap 2: " << available[srcNode][dstNode] << endl; 
-    available[srcNode][dstNode] = available[srcNode][dstNode] - 1; 
-    available[dstNode][srcNode] = available[dstNode][srcNode] - 1;
-    //cout << "after link cap  1: " << available[srcNode][dstNode] << " link cap 2: " << available[srcNode][dstNode] << endl; 
-
-    if (algo == "SHPF"){
-      shpfHops++; // increase links it went through, for shortest hop path first 
-      shpfDelay = shpfDelay + propdelay[srcNode][dstNode]; // store delay for links traversed through 
-    }else if (algo == "SDPF"){
-      sdpfHops++; // increase links it went through, for shortest delay path first 
-      sdpfDelay = sdpfDelay + propdelay[srcNode][dstNode];; // store delay for links traversed through 
-    }else if (algo == "LLP"){
-      llpHops++; // increase links it went through, for Least Loaded path
-      llpDelay = llpDelay + propdelay[srcNode][dstNode];; // store delay for links traversed through 
     }
     srcNode = dstNode; s = d; // set to next node 
   }
+
+  srcNode1 = tmproute.top(); s = alpha[srcNode1];
+  tmproute.pop(); 
+ 
+  while(!tmproute.empty()){
+    dstNode1 = tmproute.top(); d = alpha[dstNode1];
+    tmproute.pop();
+
+    link = s + d;
+    //cout <<" Taking from Link: " << link << endl;
+    
+    // restore capacity back to link 
+    available[srcNode1][dstNode1] = available[srcNode1][dstNode1] - 1; 
+    available[dstNode1][srcNode1] = available[dstNode1][srcNode1] - 1;
+
+    if (algo == "SHPF"){
+      shpfHops++; // increase links it went through, for shortest hop path first 
+      shpfDelay += propdelay[srcNode1][dstNode1]; // store delay for links traversed through 
+    }else if (algo == "SDPF"){
+      sdpfHops++; // increase links it went through, for shortest delay path first 
+      //cout << "link delay: " << propdelay[srcNode1][dstNode1]<< endl;
+      sdpfDelay += propdelay[srcNode1][dstNode1]; // store delay for links traversed through 
+      //cout << "total delay: " << sdpfDelay<< endl;
+    }else if (algo == "LLP"){
+      llpHops++; // increase links it went through, for Least Loaded path
+      llpDelay = llpDelay + propdelay[srcNode1][dstNode1]; // store delay for links traversed through 
+    }else if (algo == "MFC"){
+      mfcHops++; // increase links it went through, for Least Loaded path
+      mfcDelay = mfcDelay + propdelay[srcNode1][dstNode1]; // store delay for links traversed through 
+    }
+
+    srcNode1 = dstNode1; s = d;
+  }
+  
 
   routesUsed.insert(pair<int, stack<int> >(currentEvent.callid, route)); 
   return 1;
@@ -135,6 +184,7 @@ void ReleaseCall(int callid)
     stack<int> route = routesUsed[callid]; 
     int srcNode = 0, dstNode=0;
     
+    //cout << "release route length: " << route.size() << endl; 
     srcNode = route.top(); s = alpha[srcNode]; // get first node 
     route.pop();
 
@@ -147,10 +197,13 @@ void ReleaseCall(int callid)
       //cout <<"Link Released: " << link << endl;
 
       // increase capacity by 1 
+      //cout << " link cap  1: " << available[srcNode][dstNode] << " link cap 2: " << available[srcNode][dstNode] << endl; 
+
       available[srcNode][dstNode] = available[srcNode][dstNode] +1; 
       available[dstNode][srcNode] = available[dstNode][srcNode] +1;
 
       srcNode = dstNode; // set to previous destination 
+      s = alpha[srcNode];
     }
   }
   else{
@@ -159,12 +212,13 @@ void ReleaseCall(int callid)
 }
 
 // Simulates all 4 policies 
-void simulatePolicy(string algo, int resource[MATRIX_SIZE][MATRIX_SIZE], list<EventList> workLoad){
+void simulatePolicy(string algo, int resource[MATRIX_SIZE][MATRIX_SIZE], list<EventList> wl){
   //Simulates the call arrivals and departures
   float time = 0.0;
-  int success = 0, blocked = 0, totalCalls = 0;
+  list<EventList> workLoad = wl; 
+  float success = 0, blocked = 0, totalCalls = 0;
   int numevents = workLoad.size();
-  //float shpfAverage = 0; 
+  float shpfAverage = 0; 
 
   while ((numevents > 0) && (!workLoad.empty()))
   {
@@ -172,7 +226,7 @@ void simulatePolicy(string algo, int resource[MATRIX_SIZE][MATRIX_SIZE], list<Ev
     EventList currentEvent = workLoad.front();
     workLoad.pop_front();
 
-    /* Get information about the current event */
+    // Get information about the current event 
     //printf("Event of type %d at time %8.6f (call %d from %c to %c)\n", currentEvent.event_type, currentEvent.event_time, currentEvent.callid, currentEvent.source, currentEvent.destination);
 
     if (currentEvent.event_type == CALL_ARRIVAL)
@@ -195,15 +249,18 @@ void simulatePolicy(string algo, int resource[MATRIX_SIZE][MATRIX_SIZE], list<Ev
     }
   }
 
+  // determine which policy to print 
+
   if (algo == "SHPF"){
-    //shpfAverage = shpfHops/success; // calculate hops for SHPF
-    cout << "SHPF\t" << totalCalls << "\t" << success <<"\t" << blocked << "\t" << shpfHops/success << "\t" << shpfDelay/success << endl;
+    cout << "SHPF\t" << totalCalls << "\t" << success <<"\t" << blocked << " ("<<  (blocked/totalCalls)*100 << "%)" << "\t" << shpfHops/success << "\t " << shpfDelay/success << endl;
   }else if(algo == "SDPF"){ 
-    cout << "SDPF\t" << totalCalls << "\t" << success <<"\t" << blocked << " ("<< blocked/100 << ")"<<"\t" << sdpfHops/success << "\t" << sdpfDelay/success << endl;
+    cout << "SDPF\t" << totalCalls << "\t" << success <<"\t" << blocked << " ("<< (blocked/totalCalls)*100 << "%)"<<"\t" << sdpfHops/success << "\t " << sdpfDelay/success << endl;
   } else if(algo == "LLP"){ 
-    cout << "LLP\t" << totalCalls << "\t" << success <<"\t" << blocked << " ("<< blocked/100 << ")"<<"\t " << llpHops/success << "\t" << llpDelay/success << endl;
+    cout << "LLP\t" << totalCalls << "\t" << success <<"\t" << blocked << " ("<< (blocked/totalCalls)*100 << "%)"<<"\t" << llpHops/success << "\t " << llpDelay/success << endl;
+  } else if(algo == "MFC"){ 
+    cout << "MFC\t" << totalCalls << "\t" << success <<"\t" << blocked << " ("<< (blocked/totalCalls)*100 << "%) "<<"\t " << mfcHops/success << "\t" << mfcDelay/success << endl;
   } 
-  
+
 }
 
 int main()
@@ -221,6 +278,7 @@ int main()
     // row and column position 
     row = src - 'A';
     col = dst - 'A';
+
     // fill in matrix with capacity, delay and cost information
     propdelay[row][col] = delay;
     propdelay[col][row] = delay;
@@ -228,6 +286,8 @@ int main()
     capacity[col][row] = cap;
     available[row][col] = cap;
     available[col][row] = cap;
+    reset[row][col] = cap;
+    reset[col][row] = cap;
     cost[row][col] = 1;
     cost[col][row] = 1;
   }
@@ -252,7 +312,7 @@ int main()
 
     // add end call event, for the created event
     EventList tmpEvent2;
-    tmpEvent2.event_time = arr + dur; // length of call
+    tmpEvent2.event_time = tmpEvent.event_time + tmpEvent.duration; // length of call
     tmpEvent2.event_type = CALL_END;
     tmpEvent2.callid = id;
     tmpEvent2.source = src;
@@ -268,9 +328,17 @@ int main()
   workLoad.sort(compareEvent);
 
   //Print final report for each policy 
-  cout << "Policy\tCalls\tSuccess\tBlocked(%)\tHops\tDelay"<< endl;
-  cout <<"  -----------------------------------------------" << endl; 
+  cout << "Policy\tCalls\tSuccess\tBlocked(%)\tHops\t  Delay"<< endl;
+  cout <<"  ---------------------------------------------------" << endl; 
   simulatePolicy("SHPF", cost, workLoad); // Run Simulation for Short Hop Path First
+  routesUsed.clear(); // clear map for next round 
+
   simulatePolicy("SDPF", propdelay, workLoad); // Run Simulation for Short Delay Path First 
+  routesUsed.clear(); // clear map for next round 
+
   simulatePolicy("LLP", available, workLoad); // Run Simulation for Short Delay Path First
+  routesUsed.clear(); // clear map for next round 
+
+  //simulatePolicy("MFC", available, workLoad); // Run Simulation for Maximum free circuits
+  //routesUsed.clear(); // clear map for next round 
 }
